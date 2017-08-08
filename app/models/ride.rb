9ff6693de3b3f5959269f3de0df944d7afbac2bf
Samson Yuwono <samson.yuwono@gmail.com>
2017-08-08 23:33:17 +0000

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,53 @@
 class Ride < ActiveRecord::Base
-  # write associations here
+
+  belongs_to :attraction
+  belongs_to :user
+
+  def take_ride
+    if enough_tickets? && tall_enough?
+      ride_accepted
+    else
+      print_issue
+    end
+  end
+
+  def ride_accepted
+    update_tickets
+    update_nausea
+    update_happiness
+    self.user.save
+  end
+
+  def enough_tickets?
+     self.user.tickets >= self.attraction.tickets
+  end
+
+  def tall_enough?
+    self.user.height >= self.attraction.min_height
+  end
+
+  def update_tickets
+    self.user.tickets -= self.attraction.tickets
+  end
+
+  def update_nausea
+    self.user.nausea += self.attraction.nausea_rating
+  end
+
+  def update_happiness
+    self.user.happiness += self.attraction.happiness_rating
+  end
+
+  def ticket_issue
+    enough_tickets? ? "" : " You do not have enough tickets to ride the #{attraction.name}."
+  end
+
+  def height_issue
+    tall_enough? ? "" : " You are not tall enough to ride the #{attraction.name}."
+  end
+
+  def print_issue
+    "Sorry." + ticket_issue + height_issue
+  end
+
 end
