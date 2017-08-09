a08bb6f860b6ae4022b0b53fd1755d4354cbba1c
Samson Yuwono <samson.yuwono@gmail.com>
2017-08-09 15:58:54 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
+require 'pry'
+
+  def index
+    @users = User.all
+  end
 
   def new
+    @user= User.new
   end
 
   def create
@@ -9,16 +15,16 @@ class UsersController < ApplicationController
       session[:user_id]= @user.id
       redirect_to user_path(@user)
     else
-      redirect_to '/'
+      redirect_to signup_path
     end
   end
 
   def show
     @user = User.find_by(params[:id])
     if session[:user_id]
-      redirect_to user_path(@user)
+      render :show
     else
-      redirect_to '/'
+      redirect_to root_path
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   root 'application#home'
-  
+
   resources :attractions
   resources :users
   resources :sessions
   resources :rides
+
+
+  get '/signin' => 'sessions#new'
+  get '/users/new' => 'users#new', as: 'signup'
 
 
 end
