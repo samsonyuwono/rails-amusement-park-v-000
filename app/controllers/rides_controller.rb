ea7c64a2880e74ae41343c8a5b71199d774f264e
Samson Yuwono <samson.yuwono@gmail.com>
2017-08-10 20:32:49 +0000

    Done.

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,16 +1,14 @@
 class RidesController < ApplicationController
 
   def new
-    @ride = Ride.create(:user_id=> current_user.id, :attraction_id=> params[:attraction_id])
-      if @ride.take_ride
-      redirect_to user_path(current_user)
+    ride = Ride.create(
+    user_id: current_user.id,
+    attraction_id: params[:attraction_id]
+    )
+    redirect_to user_path(current_user), notice: ride.take_ride
+
     end
-  end
 
-  private
 
-  def ride_params
-    params.require(:ride).permit(:user_id, :attraction_id)
-  end
 
 end
